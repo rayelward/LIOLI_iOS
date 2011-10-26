48a6d61fca9a7a1cb63c1759c8e086c26c0b579c
Raymond Elward <rayelward@gmail.com>
2011-10-25 22:13:33 -0500

    Added some comments testing git conflict from desktop.

--- LIOLI_2/FirstViewController.h
+++ LIOLI_2/FirstViewController.h
@@ -4,7 +4,8 @@
 //
 //  Created by Raymond Elward on 10/16/11.
 //  Copyright (c) 2011 Raymond Elward. All rights reserved.
-//
+//  This is a test .  need to refactor this view into the 
+//  RecentsViewController.
 
 #import <UIKit/UIKit.h>
 
