65e7774c38a67d2cbcb6ba39088669cdf3a1d644
Raymond Elward <rayelward@gmail.com>
2011-11-19 14:51:27 -0600

    Changed recent view controller to a tableview.  Added some comments to the helper.  updated the storyboard.

--- LIOLI_2/RecentsViewController.h
+++ LIOLI_2/RecentsViewController.h
@@ -1,14 +1,13 @@
 //
-//  FirstViewController.h
+//  RecentsViewController.h
 //  LIOLI_2
 //
-//  Created by Raymond Elward on 10/16/11.
+//  Created by Raymond Elward on 11/19/11.
 //  Copyright (c) 2011 Raymond Elward. All rights reserved.
-//  This is a test .  need to refactor this view into the 
-//  RecentsViewController.
+//
 
 #import <UIKit/UIKit.h>
 
-@interface RecentsViewController : UIViewController
+@interface RecentsViewController : UITableViewController
 
 @end
--- LIOLI_2/lioliHelper.m
+++ LIOLI_2/lioliHelper.m
@@ -12,23 +12,23 @@
 
 
 
-
+/*
+ * Works but does a sync call for the most recent postings.
+ */
 +(NSArray *)getRecentTen: (NSNumber *) page
 {
     NSString *callingUrl = [NSString stringWithFormat:@"recents/%@", page];
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:URL_TEMPLATE, callingUrl]];
     NSArray *result = 
         (NSArray *)[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:url] 
-                                                        options:NSJSONReadingMutableContainers 
-                                                          error:nil];
-    NSLog(@"body:%@", [[result objectAtIndex:2] objectForKey:@"body"]);
-    NSLog(@"id:%@", [[result objectAtIndex:2] objectForKey:@"unique_id"]);
-    NSLog(@"love:%@", [[result objectAtIndex:2] objectForKey:@"loves"]);
-    NSLog(@"leave:%@", [[result objectAtIndex:2] objectForKey:@"leaves"]);
-    NSLog(@"gender:%@", [[result objectAtIndex:2] objectForKey:@"gender"]);
-    NSLog(@"age:%@", [[result objectAtIndex:2] objectForKey:@"age"]);
+                                                   options:NSJSONReadingMutableContainers 
+                                                     error:nil];
     return result;
 }
+
+/*
+ * Brings in 10 random entries for the user to vote on.
+ */
 +(NSArray *)getRandomTen
 {
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:URL_TEMPLATE, @"random_ten"]];
@@ -36,9 +36,11 @@
     (NSArray *)[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:url] 
                                                options:NSJSONReadingMutableContainers 
                                                  error:nil];
-    NSLog(@"getRandomTen: %@", result);
     return result;
 }
+/*
+ * TODO: send data to the server and get the unique id back.
+ */
 +(NSNumber *)sendPost:(NSDictionary *) story
 {
     NSString *callingUrl = [NSString stringWithFormat:@"submit/"];
@@ -50,9 +52,12 @@
     NSLog(@"%@", result);
     return nil;
 }
+/*
+ * Adds to the number of loves for a specific id.  Returns the new loves or wrong id
+ */
 +(bool)addLoves:(NSNumber *)uniqueId
 {
-    NSString *callingUrl = [NSString stringWithFormat:@"add_leaves/%@", uniqueId];
+    NSString *callingUrl = [NSString stringWithFormat:@"add_loves/%@", uniqueId];
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:URL_TEMPLATE, callingUrl]];
     NSString *result = [[NSString alloc] initWithContentsOfURL:url 
                                                       encoding:NSUTF8StringEncoding 
@@ -60,9 +65,12 @@
     NSLog(@"%@", result);
     return NO;
 }
+/*
+ * Adds to the number of leaves for a specific id.  Returns the new loves or wrong id
+ */
 +(bool)addLeaves:(NSNumber *)uniqueId
 {
-    NSString *callingUrl = [NSString stringWithFormat:@"add_loves/%@", uniqueId];
+    NSString *callingUrl = [NSString stringWithFormat:@"add_leaves/%@", uniqueId];
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:URL_TEMPLATE, callingUrl]];
     NSString *result = [[NSString alloc] initWithContentsOfURL:url 
                                                       encoding:NSUTF8StringEncoding 
@@ -71,6 +79,9 @@
     
     return NO;
 }
+/*
+ * Gets a specific entry and returns it as a dictionary or wrong id
+ */
 +(NSDictionary *)getEntry:(NSNumber *)uniqueId
 {
     NSString *callingUrl = [NSString stringWithFormat:@"get_entry/%@", uniqueId];
